7bd2c462feb010003fb2f2917fb98ac2f81c5b2b
devYuraKim <dev.yurakim@gmail.com>
2025-08-26 21:11:44 +0900

    fix: replace validation constraints not applicable to numeric types (NotEmpty, Pattern for String)

--- accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
@@ -1,14 +1,17 @@
 package com.example.accounts.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class AccountsDto {
 
-    @NotEmpty(message="Account number is required")
-    @Pattern(regexp = "^\\d{10}$", message="Account number must be 10 digits")
+    //@NotEmpty(message="Account number is required")
+    //@Pattern(regexp = "^\\d{10}$", message="Account number must be 10 digits")
+    @NotNull(message="Account number is required")
     private Long accountNumber;
 
     @NotEmpty(message="Account type is required")
