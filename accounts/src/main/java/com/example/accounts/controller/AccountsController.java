acb7e70a98b64719b3ffc7534eee35bff496ec66
devYuraKim <dev.yurakim@gmail.com>
2025-08-26 14:57:38 +0900

    refactor: extract successResponseBuilder and unify controller method structure

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -23,46 +23,31 @@ public class AccountsController {
 
     private final IAccountsService iAccountsService;
 
+    private ResponseEntity<ResponseDto> buildSuccessResponse(HttpStatus httpStatus, String status, String message){
+        return ResponseEntity.status(httpStatus).body(new ResponseDto(status, message));
+    };
+
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
-
         iAccountsService.createAccount(customerDto);
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+        return buildSuccessResponse(HttpStatus.CREATED, AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201);
     }
 
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
-
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(customerDto);
+        return ResponseEntity.ok(customerDto);
     }
 
-    //TODO: remove boolean flag and handle exception with try-catch
     @PostMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
-
-        boolean isUpdated = iAccountsService.updateAccount(customerDto);
-        if(isUpdated) {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
-        }else{
-            return ResponseEntity
-                    .status(HttpStatus.EXPECTATION_FAILED)
-                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
-        }
+        iAccountsService.updateAccount(customerDto);
+        return buildSuccessResponse(HttpStatus.OK, AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200);
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
         iAccountsService.deleteAccount(mobileNumber);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        return buildSuccessResponse(HttpStatus.OK, AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200);
     }
 }
