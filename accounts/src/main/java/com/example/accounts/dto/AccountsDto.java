b95d01ba566aeac42c830b8eecac91ca9a16986e
devYuraKim <dev.yurakim@gmail.com>
2025-08-26 20:27:19 +0900

    fix: set input validation with constraint annotations

--- accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
@@ -1,11 +1,19 @@
 package com.example.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message="Account number is required")
+    @Pattern(regexp = "^\\d{10}$", message="Account number must be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message="Account type is required")
     private String accountType;
+
+    @NotEmpty(message="Branch address is required")
     private String branchAddress;
 }
--- accounts/src/main/java/com/example/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/example/accounts/dto/CustomerDto.java
@@ -1,12 +1,23 @@
 package com.example.accounts.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message="Name is required")
+    @Size(min = 2, max = 50, message="Name should be between 2 and 50 characters")
     private String name;
+
+    @NotEmpty(message="Email is required")
+    @Email(message="Email should be valid")
     private String email;
+
+    @Pattern(regexp = "^\\d{11}$", message="Mobile number must be 11 digits")
     private String mobileNumber;
+
+    @Valid
     private AccountsDto accountsDto;
 }
