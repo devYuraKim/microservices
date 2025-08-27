c545e6c8526dec5d5ddb788e13b28f52e2e30471
devYuraKim <dev.yurakim@gmail.com>
2025-08-27 20:26:29 +0900

    refactor: remove unused import statements

--- accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
@@ -3,9 +3,7 @@ package com.example.accounts.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 @Schema(
     name ="Accounts",
