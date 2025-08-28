f264d14b3357517bb9990636bcaa5c51be12654f
devYuraKim <dev.yurakim@gmail.com>
2025-08-28 17:15:29 +0900

    chore: apply new ApiResponseDto to controller and exception handler

--- accounts/src/main/java/com/example/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/example/accounts/exception/GlobalExceptionHandler.java
@@ -1,80 +1,83 @@
 package com.example.accounts.exception;
 
-import com.example.accounts.dto.ErrorResponseDto;
+import com.example.accounts.dto.ApiResponseDto;
+import com.example.accounts.util.ApiResponseBuilder;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-// TODO[API]: Switch to ApiResponse<T> to unify success/error response structure
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    //extract a private helper method for building error responses
-    private ResponseEntity<ErrorResponseDto> buildErrorResponse(
-            WebRequest webRequest,
-            Exception exception,
-            HttpStatus httpStatus
-    ) {
-        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-            webRequest.getDescription(false),
-            httpStatus,
-            exception.getMessage(),
-            LocalDateTime.now()
-        );
-        return new ResponseEntity<>(errorResponseDTO, httpStatus);
-    }
-
+    //400 BAD REQUEST - handles manually thrown errors (e.g., AccountServiceImpl updateAccount() customerDto == null)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(
-            WebRequest webRequest, IllegalArgumentException exception
-    ) {
-        return buildErrorResponse(webRequest, exception, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponseDto<Void>> handleIllegalArgumentException(WebRequest webRequest, IllegalArgumentException exception) {
+        return ApiResponseBuilder.buildErrorResponse(HttpStatus.BAD_REQUEST, webRequest, exception);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDto> handleGlobalException(WebRequest webRequest, Exception exception) {
-        return buildErrorResponse(webRequest, exception, HttpStatus.INTERNAL_SERVER_ERROR);
+    //TODO: Look into the classification and handling of validation exceptions and how constraint violations are collected and wrapped in the unified API response
+    //MethodArgumentNotValid: Has a default handler in ResponseEntityExceptionHandler called handleMethodArgumentNotValid
+    //ConstraintViolationException: Has no default handler
+
+    //400 BAD REQUEST
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponseDto<Map<String, String>>> handleConstraintViolationException(WebRequest webRequest, ConstraintViolationException exception) {
+        Map<String, String> errors = exception.getConstraintViolations().stream()
+                .collect(Collectors.toMap(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                ));
+
+        return ApiResponseBuilder.buildValidationErrorResponse(errors, webRequest);
     }
 
-    @ExceptionHandler(CustomerAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(
-            WebRequest webRequest, CustomerAlreadyExistsException exception
-    ) {
-        return buildErrorResponse(webRequest, exception, HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(
-            WebRequest webRequest, ResourceNotFoundException exception
-    ) {
-        return buildErrorResponse(webRequest, exception, HttpStatus.NOT_FOUND);
-    }
-
-    //TODO: add @ExceptionHandler(ConstraintViolationException.class)
-    //NOTE: this method handles exceptions only triggered by @Valid on @RequestBody
+    //400 BAD REQUEST
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        Map<String, String> validationErrorsMap = new HashMap<>();
-        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
-
-        validationErrorList.forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String validationMessage = error.getDefaultMessage();
-            validationErrorsMap.put(fieldName, validationMessage);
-        });
-        return ResponseEntity.badRequest().body(validationErrorsMap);
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders _headers,
+            HttpStatusCode _status,
+            WebRequest request
+    ) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        FieldError::getDefaultMessage
+                ));
+        return (ResponseEntity<Object>) (ResponseEntity<?>) ApiResponseBuilder.buildValidationErrorResponse(errors, request);
     }
+
+    //409 CONFLICT
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ApiResponseDto<Void>> handleCustomerAlreadyExistsException(WebRequest webRequest, CustomerAlreadyExistsException exception) {
+        return ApiResponseBuilder.buildErrorResponse(HttpStatus.CONFLICT, webRequest, exception);
+    }
+
+    //404 NOT FOUND
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponseDto<Void>> handleResourceNotFoundException(WebRequest webRequest, ResourceNotFoundException exception) {
+        return ApiResponseBuilder.buildErrorResponse(HttpStatus.NOT_FOUND, webRequest, exception);
+    }
+
+    //500 INTERNAL SERVER ERROR
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDto<Void>> handleGlobalException(WebRequest webRequest, Exception exception) {
+        log.error("Unhandled exception at {}: {}", webRequest.getDescription(false), exception.getMessage(), exception);
+        return ApiResponseBuilder.buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, webRequest, exception);
+    }
+
 }
