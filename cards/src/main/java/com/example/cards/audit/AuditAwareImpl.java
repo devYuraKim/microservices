3973b2bbc70812ee5bc1d651f760ff5ae71c1d43
devYuraKim <dev.yurakim@gmail.com>
2025-08-29 16:07:04 +0900

    Revert "feat: Cards microservices"
    This reverts commit c995f61ddfc1e314cdcebfd015f05c6550ed2c5a.

---
+++ cards/src/main/java/com/example/cards/audit/AuditAwareImpl.java
@@ -0,0 +1,20 @@
+package com.example.cards.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware {
+
+    //TODO: modify the existing method to get current user from security context
+    // OR (✅recommended) create a new implementation of AuditorAware and switch using DI
+    // -option1. reuse the same component name "auditAwareImpl" for the new class
+    // -option2. use @Profile to switch between implementations (@Profile("dev") → this class, @Profile("prod") → SecurityAuditorAwareImpl)
+    // -option3. use @Primary to mark the default implementation when multiple components exist
+    @Override
+    public Optional getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
+}
